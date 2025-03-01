2ae23ce6b8caab52976edea8e28fe1208c1651b9
suborna33 <152749142+suborna33@users.noreply.github.com>
2025-03-01 17:51:12 +0600

    update

--- script/date.js
+++ script/date.js
@@ -1,7 +1,7 @@
 //
 function updateDate() {
     const now = new Date();
-    const options = { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' };
+    const options = { weekday: 'short',  month: 'short', day: 'numeric', year: 'numeric' };
     const formattedDate = now.toLocaleDateString('en-US', options);
     const finalDate = formattedDate.replace(',', '');
     
